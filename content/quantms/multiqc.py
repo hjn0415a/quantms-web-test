e6ba97686b8f43b241b6f2c018c69dcca5fc5092
hjn0415a <hjn0415a@gmail.com>
2025-09-15 13:47:10 +0900

    fix: update path to multiqc_report.html

--- content/quantms/multiqc.py
+++ content/quantms/multiqc.py
@@ -6,7 +6,11 @@ from src.common.common import page_setup
 
 params = page_setup()
 
-html_path = Path("/app/results/multiqc_report.html")
+results_dir = Path(st.session_state.workspace, "results")
+html_path = results_dir /"summarypipeline" / "multiqc_report.html"
 
-html_content = html_path.read_text(encoding="utf-8")
-components.html(html_content, height=800, scrolling=True)
+if html_path.exists():
+    html_content = html_path.read_text(encoding="utf-8")
+    components.html(html_content, height=800, scrolling=True)
+else:
+    st.warning("MultiQC report not found. Please run the analysis first.")
