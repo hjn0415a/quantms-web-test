01d33fd05a13e6b6eb20d4fcdf64eea824dd6fdb
Tom David Mueller <tom.mueller@beachouse.de>
2025-11-08 17:55:30 +0100

    deconfuse download logic

--- content/quantms/quantms_download.py
+++ content/quantms/quantms_download.py
@@ -9,16 +9,13 @@ st.title("QuantMS Analysis Download Page")
 results_dir = Path(st.session_state.workspace, "results")
 zip_path = results_dir.with_suffix(".zip")
 
-if st.session_state.get("analysis_success") or zip_path.is_file():
-    if zip_path.is_file():
-        with open(zip_path, "rb") as f:
-            st.download_button(
-                label="📦 Download Results (.zip)",
-                data=f,
-                file_name=zip_path.name,
-                mime="application/zip"
-            )
-    else:
-        st.warning("Result zip file not found.")
+if zip_path.is_file():
+    with open(zip_path, "rb") as f:
+        st.download_button(
+            label="📦 Download Results (.zip)",
+            data=f,
+            file_name=zip_path.name,
+            mime="application/zip"
+        )
 else:
     st.info("Results will be available for download once the analysis is successfully completed.")
